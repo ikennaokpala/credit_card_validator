0762c80ddfc1eff9c3b95ebcedf89feee166cb26
Ikenna Okpala <ikennaokpala@gmail.com>
2013-02-25 17:32:12 +0000

    Implement credit card validator

--- lib/credit_card_validator.rb
+++ lib/credit_card_validator.rb
@@ -1,8 +1,18 @@
 require "credit_card_validator/version"
 
+require 'credit_card_validator/payment_method'
+require 'credit_card_validator/payment_method/credit_card'
+require 'credit_card_validator/payment_method/amex'
+require 'credit_card_validator/payment_method/discover'
+require 'credit_card_validator/payment_method/master_card'
+require 'credit_card_validator/payment_method/visa'
+
 module CreditCardValidator
 
   def self.validate(number)
+    if credit_card = PaymentMethod.credit_card_from_number(number.to_s.gsub(/[^\d]/, ''))
+      "#{credit_card.name}: #{credit_card.number} (#{credit_card.validity})"
+    end
   end
 
 end
